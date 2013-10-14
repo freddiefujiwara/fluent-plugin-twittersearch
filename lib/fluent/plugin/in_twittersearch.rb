74c4660a9776554a7a4a4e45d55a4dc372f54359
Freddie Fujiwara <github@ze.gs>
2013-10-14 15:44:24 +0900

    json-api -> twitter

--- lib/fluent/plugin/in_twittersearch.rb
+++ lib/fluent/plugin/in_twittersearch.rb
@@ -3,48 +3,18 @@ module Fluent
     end
     class TwittersearchInput < Input
         Plugin.register_input('twittersearch', self)
+        config_param :consumer_key, :string
+        config_param :consumer_secret, :string
+        config_param :oauth_token, :string
+        config_param :oauth_token_secret, :string
+        config_param :tag, :string
+        config_param :keyword, :string
 
-        config_param :url,      :string,  :default => 'http://pipes.yahoo.com/pipes/pipe.run?_id=c9b9df32b4c3e0ccbe4547ae7e00ed2f&_render=json&condition=d7D&genre=100533&page=__PAGE__'
-        config_param :rules   , :string
-        config_param :sleep   , :integer, :default =>  0
-        config_param :tag,      :string
-
-        attr_reader :urls
         def initialize
             super
             require 'json'
             require 'net/http'
             @urls = []
-        end
-
-        def configure(config)
-            super
-
-            begin
-                result = []
-                (eval config['rules']).each_pair do |key,replecements|
-                    combinations = []
-                    (replecements.to_a).each do |replacement|
-                        combinations << {key => replacement}
-                    end 
-                    result << combinations
-                end
-
-                @rules = result[0].product(*result[1..-1])
-            rescue SyntaxError,StandardError => e 
-                raise Fluent::ConfigError.new "rules has some errors #{e}"
-            end
-
-            raise Fluent::ConfigError unless ['http','https'].include? URI.parse(config['url']).scheme
-            @rules.each do |pair|
-                url = config['url']
-                pair.each do |key_replacements|
-                    key_replacements.each do |key,replacement|
-                        url = url.gsub(key.to_s,replacement.to_s)
-                    end
-                end
-                @urls.push url
-            end
         end
 
         def start
--- test/plugin/in_twittersearch.rb
+++ test/plugin/in_twittersearch.rb
@@ -7,12 +7,13 @@ class FileInputTest < Test::Unit::TestCase
         Fluent::Test.setup
 
         @d = create_driver %[
-            url   http://pipes.yahoo.com/pipes/pipe.run?_id=c9b9df32b4c3e0ccbe4547ae7e00ed2f&_render=json&condition=d7D&genre=__GENRE__&page=__PAGE__
-            rules   {'__PAGE__' => (1..10),'__GENRE__' => [1,100533]} 
-            sleep       1
-            tag     input.json
+            consumer_key        CONSUMER_KEY
+            consumer_secret     CONSUMER_SECRET
+            oauth_token         OAUTH_TOKEN
+            oauth_token_secret  OAUTH_TOKEN_SECRET
+            tag                 input.twitter
+            keyword             sampling
         ]
-        @time = Time.now.to_i
     end
 
     def create_driver(config = CONFIG)
@@ -20,91 +21,18 @@ class FileInputTest < Test::Unit::TestCase
     end
 
     def test_configure
-        assert_equal 'http://pipes.yahoo.com/pipes/pipe.run?_id=c9b9df32b4c3e0ccbe4547ae7e00ed2f&_render=json&condition=d7D&genre=__GENRE__&page=__PAGE__'   , @d.instance.url
-        assert_equal @d.instance.rules.size, 20 #10(page) x 2(genre)
-        assert_equal 1           , @d.instance.sleep
-        assert_equal 'input.json', @d.instance.tag
-        [ %[
-                url   hoge
-                rules   {'__PAGE__' => (1..10)}
-                sleep       1
-                tag     input.json
-            ], #url wrong
-          %[
-                url   ftp://hoge.com
-                rules   {'__PAGE__' => (1..10)}
-                sleep       1
-                tag     input.json
-            ], #url must start at http
-          %[
-                url   http://hoge.com
-                sleep       1
-                rules   {'__PAGE__' => (1..10)}
-            ], #no tag
-          %[
-                url   http://hoge.com
-                sleep       1
-                rules   {__PAGE__}
-                tag     input.json
-            ], #rules must be {'key' => array}
-          %[
-                url   http://hoge.com
-                sleep       1
-                rules   (1..2)
-                tag     input.json
-            ], #rules must be {'key' => array}
-        ].each do |config|
-            assert_raise Fluent::ConfigError do
-                create_driver config
-            end
-        end
-    end
-
-    def test_urls
-        assert_equal @d.instance.urls.size, 20
-        index = 0
-        (1..10).each do |page|
-            [1,100533].each do |genre|
-                assert_equal "http://pipes.yahoo.com/pipes/pipe.run?_id=c9b9df32b4c3e0ccbe4547ae7e00ed2f&_render=json&condition=d7D&genre=#{genre}&page=#{page}", @d.instance.urls[index]
-
-                index += 1
-            end
-        end
-    end
-
-    def test_crawls
-        for page in 1..1
-            url = "http://pipes.yahoo.com/pipes/pipe.run?_id=c9b9df32b4c3e0ccbe4547ae7e00ed2f&_render=json&condition=d7D&genre=100533&page=#{page}"
-            response = @d.instance.crawl url
-            assert_not_nil response
-            assert_equal response['count'], 30
-            assert_equal response['value']['items'].size, 30
-            response['value']['items'].each do |item|
-                #content
-                assert item['author']['content'].to_f >= 0.0
-                assert item['author']['content'].to_f <= 5.0
-
-                #link
-                assert_equal URI.parse(item['link']).scheme, 'http' if item['link']
-                #title
-                assert_equal URI.parse(item['title']).scheme, 'http' if item['title']
-
-                #description
-                assert_not_nil item['description']
-
-                #pubDate
-                assert_not_nil item['pubDate']['content']
-            end
-        end
-
-        #parse error
-        assert_raise JSON::ParserError do
-            @d.instance.crawl 'http://www.rakuten.co.jp'
-        end
-
-        #404 Not found
-        assert_raise Fluent::TwittersearchError do
-            @d.instance.crawl 'http://www.google.com/404'
-        end
+        d = create_driver %[
+          consumer_key        CONSUMER_KEY
+          consumer_secret     CONSUMER_SECRET
+          oauth_token         OAUTH_TOKEN
+          oauth_token_secret  OAUTH_TOKEN_SECRET
+          tag                 input.twitter
+          keyword             sampling
+        ]
+        d.instance.inspect
+        assert_equal 'CONSUMER_KEY', d.instance.consumer_key
+        assert_equal 'CONSUMER_SECRET', d.instance.consumer_secret
+        assert_equal 'OAUTH_TOKEN', d.instance.oauth_token
+        assert_equal 'OAUTH_TOKEN_SECRET', d.instance.oauth_token_secret
     end
 end
